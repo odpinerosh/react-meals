8cf28067b60dfbb136e40a8f641e79a9d8eb26bb
odpinerosh <odpinerosh@gmail.com>
2022-10-08 10:00:55 -0500

    finished 149. Using the effect hook

--- src/components/Layout/HeaderCartBtn.js
+++ src/components/Layout/HeaderCartBtn.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, {useEffect, useContext, useState} from "react";
 import CartIcon from '../Cart/CartIcon';
 import CartContext from "../../store/cart-context";
 import './HeaderCartBtn.css';
 
 const HeaderCartBtn = props => {
+    const [btnIsHigh, setBtnHigh] = useState(false);
+    const cartContext = useContext(CartContext);
 
-    const cartContext = React.useContext(CartContext);
+    const { items } = cartContext;
 
-    const numberOfCartItems = cartContext.items.reduce((curr, e) => {
+    const numberOfCartItems = items.reduce((curr, e) => {
         return curr + e.amount;
     }, 0);
 
+    const btnClasses = `header-btn ${btnIsHigh ? 'bump' : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+
+        setBtnHigh(true);
+        const timer = setTimeout(() => {
+            setBtnHigh(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+        
+    }, [items]);
+
     return (
-        <button className='header-btn' onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className='header-btn-icon'><CartIcon /></span>
             <span>{props.title}</span>
             <span className='header-btn-items'>{numberOfCartItems}</span>
